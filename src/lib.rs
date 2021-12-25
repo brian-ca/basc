f1d983b2fca2ca3313b77f6eff865ddf1f9a9b96
Brian Strecker <brian.strecker@checkersautomation.net>
2021-12-25 06:43:32 -0600

    building PriceSeries from http request

--- src/lib.rs
+++ src/lib.rs
@@ -2,46 +2,55 @@
 //
 // . library to load stock price informaiton from the yahoo api
 //
-use yahoo_finance_api as yahoo;
+//use yahoo_finance_api as yahoo;
+use yahoo_finance_api::{Quote, YahooError, YahooConnector, YResponse};
 use chrono::{DateTime, Utc};
 
-#[derive(Debug, Clone, Copy)]
-struct Quote {
-    timestamp: i64,
-    high: f32,
-    low: f32,
-    open: f32,
-    close: f32,
-    adjclose: f32,
-    volume: u64
-}
+// #[derive(Debug, Clone, Copy)]
+// struct Quote {
+//     timestamp: i64,
+//     high: f32,
+//     low: f32,
+//     open: f32,
+//     close: f32,
+//     adjclose: f32,
+//     volume: u64
+// }
 
 #[derive(Debug)]
 pub struct PriceSeries {
     ticker: String,
-    quotes: Box<[Quote]>,
+    quotes: Vec<Quote>,
     start: DateTime<Utc>,
     end: DateTime<Utc>
 }
 
 impl PriceSeries {
-    pub fn new(ticker: &str, start: DateTime<Utc>, end: DateTime<Utc>) -> Result<PriceSeries, yahoo::YahooError> {
+    pub fn new(ticker: &str, start: DateTime<Utc>, end: DateTime<Utc>) -> Result<PriceSeries, YahooError> {
         Ok(PriceSeries{
             ticker: String::from(ticker),
-            quotes: Box::new([
-                Quote{
-                    timestamp: 1,
-                    high: 12.0,
-                    low: 8.0,
-                    open: 10.0,
-                    close: 10.0,
-                    adjclose: 10.0,
-                    volume: 12
-                }
-            ]),
+            quotes: Vec::new(),
             start,
             end 
         })
+
+    }
+
+    pub fn from_range(ticker: &str, start: DateTime<Utc>, end: DateTime<Utc>) -> 
+        Result<PriceSeries, YahooError> {
+        
+        let provider = YahooConnector::new();
+        let reply: YResponse = provider.get_quote_history(ticker, start, end)?;
+        let quotes: Vec<Quote> = reply.quotes()?;
+        
+        println!("quotes: {:?}", quotes);
+        Ok(PriceSeries{
+            ticker: String::from(ticker),
+            quotes,
+            start,
+            end 
+        })
+
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use basc::PriceSeries;
 
 fn main() {
 
-    let start: DateTime<Utc> = DateTime::parse_from_rfc3339("2020-12-19T16:39:57-08:00").unwrap().into();
-    let end: DateTime<Utc> = DateTime::parse_from_rfc3339("2020-12-19T16:39:57-08:00").unwrap().into();
+    let start: DateTime<Utc> = DateTime::parse_from_rfc3339("2020-12-19T00:00:00-07:00").unwrap().into();
+    let end: DateTime<Utc> = DateTime::parse_from_rfc3339("2020-12-23T23:59:58-07:00").unwrap().into();
     
     
-    if let Ok(series) = PriceSeries::new("hi", start, end) {
+    if let Ok(series) = PriceSeries::from_range("AAPL", start, end) {
         println!("got {:?}", series);
     }
 }
